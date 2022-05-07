f6b57c3286beb0a03f1e78378ca06f76132b1ebf
Liono02 <ezequiel.kippeset12@gmail.com>
2022-05-07 16:37:35 -0300

    Update script-tp1.sh

--- script-tp1.sh
+++ script-tp1.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "Bienvenido!"
-Password=$(perl -e 'crypt(const char "key", "pass1234" *salt)')
-Password2=$(echo "pass1234" | openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt -pass pass:'pass1234')
+pass=$(perl -e 'crypt("pass1234", "key")')
 for user in "$@"
 do
-    sudo useradd $user -m -p $Password2 | openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -pass pass:"pass1234"
+    sudo useradd $user -m -p $pass
     echo "Usuario: $user";
     echo "Creacion de usuaario exitosa!";
 done
